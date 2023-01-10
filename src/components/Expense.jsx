7a403e57b8845923a1a1c2d9cd4f7bb7cf0752f3
Vesku2000 <valtterisyrjanen@gmail.com>
2023-01-10 13:17:28 +0200

    new files

---
+++ src/components/Expense.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ExpenseItem from './ExpenseItem';
+
+import './Expense.css'
+
+const Expense = () => {
+  return (
+    <div>Expense</div>
+  )
+}
+
+export default Expense
